de641795b61919f4595cad7e08d22a96d07b5a47
Zain <zain.sohail@gmail.com>
2024-08-04 16:42:59 +0200

    updated created navigation to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ApolloProvider } from "@apollo/client";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import client from "./apollo-client";
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <div className="App">Hello World!</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 };
