7a13f655e6303c512a7cf9ca9be25e71250d1c25
Zain <zain.sohail@gmail.com>
2024-08-04 16:43:30 +0200

    updated apollo client to handle network errors and graphqlErrors

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, from, HttpLink } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      );
+    });
+  }
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: "https://interview-apixx07.dev.park-depot.de/",
+});
 
 const client = new ApolloClient({
-  uri: "https://interview-apixx07.dev.park-depot.de/",
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
